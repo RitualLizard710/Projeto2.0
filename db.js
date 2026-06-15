dc83aa9d2b91c996a74c345ac715dfb1f6cd9a4a
Mauricio Castro <163294847+RitualLizard710@users.noreply.github.com>
2026-06-15 15:30:41 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,21 @@
+const mysql = require("mysql2");
 
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+});
+
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error("Erro ao conectar ao MySQL:", err.message);
+        return;
+    }
+    console.log("MySQL conectado!");
+    connection.release();
+});
+
+module.exports = pool.promise();
