1ff0749d77f932b52afa1ef2601feae0fcc41a83
Mauricio Castro <163294847+RitualLizard710@users.noreply.github.com>
2026-06-15 15:35:19 -0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,20 @@
+const jwt = require("jsonwebtoken");
 
+function autenticar(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1]; // "Bearer TOKEN"
+
+    if (!token) {
+        return res.status(401).json({ erro: "Token não fornecido." });
+    }
+
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        req.usuario = payload;
+        next();
+    } catch (err) {
+        return res.status(403).json({ erro: "Token inválido ou expirado." });
+    }
+}
+
+module.exports = autenticar;
